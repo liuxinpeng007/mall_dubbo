67d38fddbdcc908c6fa491a5f219cf92dd7dc09d
liuxinpeng007 <37429848+liuxinpeng007@users.noreply.github.com>
2018-12-07 10:22:05 +0800

    1.添加mybatis逆向工程生成的类

--- mall-manager-web/src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>hello,world</title>
-</head>
-<body>
-test,my frist index jsp;
-</body>
-</html>
---
+++ mall-manager/mall-manager-dao/src/main/java/cn/mall/mapper/TbOrderShippingMapper.java
@@ -0,0 +1,30 @@
+package cn.mall.mapper;
+
+import cn.mall.pojo.TbOrderShipping;
+import cn.mall.pojo.TbOrderShippingExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderShippingMapper {
+    int countByExample(TbOrderShippingExample example);
+
+    int deleteByExample(TbOrderShippingExample example);
+
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrderShipping record);
+
+    int insertSelective(TbOrderShipping record);
+
+    List<TbOrderShipping> selectByExample(TbOrderShippingExample example);
+
+    TbOrderShipping selectByPrimaryKey(String orderId);
+
+    int updateByExampleSelective(@Param("record") TbOrderShipping record, @Param("example") TbOrderShippingExample example);
+
+    int updateByExample(@Param("record") TbOrderShipping record, @Param("example") TbOrderShippingExample example);
+
+    int updateByPrimaryKeySelective(TbOrderShipping record);
+
+    int updateByPrimaryKey(TbOrderShipping record);
+}
